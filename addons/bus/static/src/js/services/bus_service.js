435dca942e66280a206825b27aa2553a3ed131aa
zimivanende <zvanende@gmail.com>
2022-02-22 17:51:39 +0100

    [bus] Debranding + Add bookm_b.png to mail as this is used in bus

--- addons/bus/static/src/js/services/bus_service.js
+++ addons/bus/static/src/js/services/bus_service.js
@@ -1,13 +1,13 @@
-odoo.define('bus.BusService', function (require) {
-"use strict";
+odoo.define("bus.BusService", function (require) {
+  "use strict"
 
-var CrossTab = require('bus.CrossTab');
-var core = require('web.core');
-var ServicesMixin = require('web.ServicesMixin');
-const session = require('web.session');
+  var CrossTab = require("bus.CrossTab")
+  var core = require("web.core")
+  var ServicesMixin = require("web.ServicesMixin")
+  const session = require("web.session")
 
-var BusService =  CrossTab.extend(ServicesMixin, {
-    dependencies : ['local_storage'],
+  var BusService = CrossTab.extend(ServicesMixin, {
+    dependencies: ["local_storage"],
 
     // properties
     _audio: null,
@@ -19,8 +19,8 @@ var BusService =  CrossTab.extend(ServicesMixin, {
      * @param {Object} env
      */
     init: function (env) {
-        this.env = env;
-        this._super();
+      this.env = env
+      this._super()
     },
 
     /**
@@ -32,17 +32,17 @@ var BusService =  CrossTab.extend(ServicesMixin, {
      * @param {OdooEvent} ev
      */
     _trigger_up: function (ev) {
-        if (ev.name === 'call_service') {
-            const payload = ev.data;
-            let args = payload.args || [];
-            if (payload.service === 'ajax' && payload.method === 'rpc') {
-                // ajax service uses an extra 'target' argument for rpc
-                args = args.concat(ev.target);
-            }
-            const service = this.env.services[payload.service];
-            const result = service[payload.method].apply(service, args);
-            payload.callback(result);
+      if (ev.name === "call_service") {
+        const payload = ev.data
+        let args = payload.args || []
+        if (payload.service === "ajax" && payload.method === "rpc") {
+          // ajax service uses an extra 'target' argument for rpc
+          args = args.concat(ev.target)
         }
+        const service = this.env.services[payload.service]
+        const result = service[payload.method].apply(service, args)
+        payload.callback(result)
+      }
     },
     /**
      * This method is necessary in order for this Class to be used to instantiate services
@@ -65,28 +65,32 @@ var BusService =  CrossTab.extend(ServicesMixin, {
      * @param {string} [options.type] 'info', 'success', 'warning', 'danger' or ''
      */
     sendNotification(options, callback) {
-        if (window.Notification && Notification.permission === "granted") {
-            if (this.isMasterTab()) {
-                try {
-                    this._sendNativeNotification(options.title, options.message, callback);
-                } catch (error) {
-                    // Notification without Serviceworker in Chrome Android doesn't works anymore
-                    // So we fallback to displayNotification() in this case
-                    // https://bugs.chromium.org/p/chromium/issues/detail?id=481856
-                    if (error.message.indexOf('ServiceWorkerRegistration') > -1) {
-                        this.displayNotification(options);
-                        this._beep();
-                    } else {
-                        throw error;
-                    }
-                }
+      if (window.Notification && Notification.permission === "granted") {
+        if (this.isMasterTab()) {
+          try {
+            this._sendNativeNotification(
+              options.title,
+              options.message,
+              callback
+            )
+          } catch (error) {
+            // Notification without Serviceworker in Chrome Android doesn't works anymore
+            // So we fallback to displayNotification() in this case
+            // https://bugs.chromium.org/p/chromium/issues/detail?id=481856
+            if (error.message.indexOf("ServiceWorkerRegistration") > -1) {
+              this.displayNotification(options)
+              this._beep()
+            } else {
+              throw error
             }
-        } else {
-            this.displayNotification(options);
-            if (this.isMasterTab()) {
-                this._beep();
-            }
+          }
         }
+      } else {
+        this.displayNotification(options)
+        if (this.isMasterTab()) {
+          this._beep()
+        }
+      }
     },
     /**
      * Register listeners on notifications received on this bus service
@@ -95,7 +99,10 @@ var BusService =  CrossTab.extend(ServicesMixin, {
      * @param {function} func
      */
     onNotification: function () {
-        this.on.apply(this, ["notification"].concat(Array.prototype.slice.call(arguments)));
+      this.on.apply(
+        this,
+        ["notification"].concat(Array.prototype.slice.call(arguments))
+      )
     },
 
     //--------------------------------------------------------------------------
@@ -108,14 +115,16 @@ var BusService =  CrossTab.extend(ServicesMixin, {
      * @private
      */
     _beep: function () {
-        if (typeof(Audio) !== "undefined") {
-            if (!this._audio) {
-                this._audio = new Audio();
-                var ext = this._audio.canPlayType("audio/ogg; codecs=vorbis") ? ".ogg" : ".mp3";
-                this._audio.src = session.url("/mail/static/src/audio/ting" + ext);
-            }
-            Promise.resolve(this._audio.play()).catch(_.noop);
+      if (typeof Audio !== "undefined") {
+        if (!this._audio) {
+          this._audio = new Audio()
+          var ext = this._audio.canPlayType("audio/ogg; codecs=vorbis")
+            ? ".ogg"
+            : ".mp3"
+          this._audio.src = session.url("/mail/static/src/audio/ting" + ext)
         }
+        Promise.resolve(this._audio.play()).catch(_.noop)
+      }
     },
     /**
      * Show a browser notification
@@ -126,31 +135,30 @@ var BusService =  CrossTab.extend(ServicesMixin, {
      * @param {function} [callback] if given callback will be called when user clicks on notification
      */
     _sendNativeNotification: function (title, content, callback) {
-        var notification = new Notification(
-            // The native Notification API works with plain text and not HTML
-            // unescaping is safe because done only at the **last** step
-            _.unescape(title),
-            {
-                body: _.unescape(content),
-                icon: "/mail/static/src/img/odoobot_transparent.png"
-            });
-        notification.onclick = function () {
-            window.focus();
-            if (this.cancel) {
-                this.cancel();
-            } else if (this.close) {
-                this.close();
-            }
-            if (callback) {
-                callback();
-            }
-        };
+      var notification = new Notification(
+        // The native Notification API works with plain text and not HTML
+        // unescaping is safe because done only at the **last** step
+        _.unescape(title),
+        {
+          body: _.unescape(content),
+          icon: "/mail/static/src/img/bookm_b.png",
+        }
+      )
+      notification.onclick = function () {
+        window.focus()
+        if (this.cancel) {
+          this.cancel()
+        } else if (this.close) {
+          this.close()
+        }
+        if (callback) {
+          callback()
+        }
+      }
     },
+  })
 
-});
+  core.serviceRegistry.add("bus_service", BusService)
 
-core.serviceRegistry.add('bus_service', BusService);
-
-return BusService;
-
-});
+  return BusService
+})
