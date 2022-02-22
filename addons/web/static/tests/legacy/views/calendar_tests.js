f893595b9101bd55319124456a61ff19023f48cb
zimivanende <zvanende@gmail.com>
2022-02-22 12:13:42 +0100

    [web] Debrand Static tests

--- addons/web/static/tests/legacy/views/calendar_tests.js
+++ addons/web/static/tests/legacy/views/calendar_tests.js
@@ -594,7 +594,7 @@ QUnit.module('Views', {
                         message: {
                             code: 200,
                             data: {},
-                            message: "Odoo server error",
+                            message: "Server error",
                         },
                         event: event
                     });
