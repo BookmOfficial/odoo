b090f1344ef5e757c5a2facb9c5e32b4bddb3be7
zimivanende <zvanende@gmail.com>
2022-03-31 18:52:47 +0200

    [auth_oauth] Remove Odoo Auth provider

--- addons/auth_oauth/models/__init__.py
+++ addons/auth_oauth/models/__init__.py
@@ -3,5 +3,5 @@
 
 from . import auth_oauth
 from . import res_config_settings
-from . import ir_config_parameter
+# Remove Odoo Auth provider from . import ir_config_parameter
 from . import res_users
