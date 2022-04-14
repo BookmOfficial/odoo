e86d8534e265b305e463285a481cb63eae12ef20
zimivanende <zvanende@gmail.com>
2022-04-14 10:23:48 +0200

    [google_calendar] Default debranding

--- addons/google_calendar/wizard/reset_account.py
+++ addons/google_calendar/wizard/reset_account.py
@@ -15,7 +15,7 @@ class ResetGoogleAccount(models.TransientModel):
     delete_policy = fields.Selection(
         [('dont_delete', "Leave them untouched"),
          ('delete_google', "Delete from the current Google Calendar account"),
-         ('delete_odoo', "Delete from Odoo"),
+         ('delete_odoo', "Delete from Bookm"),
          ('delete_both', "Delete from both"),
         ], string="User's Existing Events", required=True, default='dont_delete',
         help="This will only affect events for which the user is the owner")
