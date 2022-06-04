32b0b75ff77a59ce6963e9894c9d46af527e01a1
zimivanende <zvanende@gmail.com>
2022-06-04 17:15:47 +0200

    fix reqs

--- setup.py
+++ setup.py
@@ -38,9 +38,9 @@ setup(
         'pillow',  # windows binary http://www.lfd.uci.edu/~gohlke/pythonlibs/
         'polib',
         'psutil',  # windows binary code.google.com/p/psutil/downloads/list
-        'psycopg2 >= 2.2',
+        'psycopg2-binary >= 2.2',
         'pydot',
-        'pypdf2',
+        'pypdf2 == 1.27.12',
         'pyserial',
         'python-dateutil',
         'python-stdnum',
@@ -51,7 +51,7 @@ setup(
         'requests',
         'zeep',
         'vobject',
-        'werkzeug',
+        'werkzeug == 0.16.1',
         'xlsxwriter',
         'xlwt',
     ],
