7a804d586afcd14a50f3700f39999a86f6f56db6
Vincent Baeten <vincent@baeten.dev>
2022-06-05 21:34:25 +0200

    [im_livechat] Basic debranding

--- addons/google_calendar/utils/google_calendar.py
+++ addons/google_calendar/utils/google_calendar.py
@@ -106,7 +106,7 @@ class GoogleCalendarService():
         readonly = '.readonly' if RO else ''
         return 'https://www.googleapis.com/auth/calendar%s' % (readonly)
 
-    def _google_authentication_url(self, from_url='http://www.odoo.com'):
+    def _google_authentication_url(self, from_url='https://www.bookm.be'):
         return self.google_service._get_authorize_uri(from_url, service='calendar', scope=self._get_calendar_scope())
 
     def _can_authorize_google(self, user):
